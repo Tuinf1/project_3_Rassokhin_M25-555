f1284987ba40fcb6436427d787ce51a3176407ad
Rassokhin Vladislav <rassokhinvladislav@mail.ru>
2026-02-03 07:16:41 +0700

    3.3 commit 25

--- test.py
+++
@@ -1,9 +0,0 @@
-import pprint
-
-from valutatrade_hub.core.usecases import get_rate
-
-try:
-    result = get_rate("USD", "EUR")
-    pprint.pprint(result)
-except Exception as e:
-    print(f"Ошибка: {e}")
